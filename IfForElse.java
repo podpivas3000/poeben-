091ff4145f1164320ccf05c9076c22a414e82a28
podpivas3000 <102377496+podpivas3000@users.noreply.github.com>
2022-03-25 19:23:56 +0300

    Add files via upload

---
+++ IfForElse.java
@@ -0,0 +1,34 @@
+public class IfForElse {
+
+
+
+        int many = 10;
+
+if(many<=10) {
+
+    System.out.println("ti bomj!");
+    }
+else if (many>10) {
+        System.out.println("ti BOGA4!");
+
+    }
+
+
+5*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
